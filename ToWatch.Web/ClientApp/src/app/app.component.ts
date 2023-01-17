d764633ea3dd3f55a123e86c92544d2aa3bc319b
Roman Danylevych <r.danylevych@brainence.com>
2023-01-17 11:13:14 +0200

    Added loader & home page

--- ToWatch.Web/ClientApp/src/app/app.component.ts
+++ ToWatch.Web/ClientApp/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      document.getElementById('loadingWrapper')['style']['opacity'] = '0'
+      setTimeout(() => {
+        document.getElementById('loadingWrapper')['style']['display'] = 'none'
+      }, 1000)
+    }, 2000)
+  }
 }
